8552bda4a8ef6205976ab660929cd5cf55ee2e89
Bedirhan <bedirhansiyam@gmail.com>
2023-03-21 17:48:19 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CharacterSwap;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter a word : ");
+        string word = Console.ReadLine();
+
+        char firstLetter = word[0];
+        char lastLetter = word[word.Length-1];
+
+        word = word.Remove(0,1);
+        word = word.Remove(word.Length-1,1);
+
+        Console.WriteLine("");
+        Console.WriteLine("Result after swap : " + lastLetter + word + firstLetter);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Character Swap - .Net Core
+In this application, the user enters a word and the application swaps the first and last character of the given word and prints it to the console.
+</br>
+
+*Sample input: Hello - Welcome - Happy* </br>
+*Sample output: oellH - eelcomW - yappH*
+
+</br>
+
+[Patika.dev](https://app.patika.dev/)
